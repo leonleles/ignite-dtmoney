25c408a04eb480c86e3b55dc5c76b473fade8d1a
leonleles <leoleles9159@hotmail.com>
2021-04-05 11:25:09 -0300

    Configura miragejs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+
+  useEffect(() => {
+    api.get('/transitions').then((response) => {
+      console.log(response);
+    });
+  }, []);
+
   return (
     <Container>
       <table>
