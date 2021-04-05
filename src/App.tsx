b0e546480a8ae02b90e89657bf349fcdf920e6bb
leonleles <leoleles9159@hotmail.com>
2021-04-05 13:46:58 -0300

    Component modal parcial

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import ReactModal from 'react-modal';
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+ReactModal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
